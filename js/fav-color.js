2e91f3e658de1dcb92264b583d9f66862c6e1a5c
Majedah Hatamleh <MAJIDAHATAMLEH@OUTLOOK.COM>
2021-03-24 14:36:05 +0200

    added images and update

--- js/fav-color.js
+++ js/fav-color.js
@@ -1,7 +1,6 @@
 'use strict'
 
 
-
 // Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
@@ -10,26 +9,24 @@ var i;
 
 // Full-width images
 function one() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "100%";
-    }
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
 }
 
 // Two images side by side
 function two() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "50%";
-    }
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
 }
 
 // Four images side by side
 function four() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.flex = "25%";
-    }
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
 }
-
-
 
 
 let formA = document.getElementById('theForm')
