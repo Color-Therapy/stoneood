6091e1aa21f437d3e47f1111638df529329cd6f0
mohsmadi <msmadi_7@yahoo.com>
2021-03-24 15:51:27 +0200

    added img and some style

--- js/index.js
+++ js/index.js
@@ -13,12 +13,12 @@ form.addEventListener('submit',clickhandler)
     function clickhandler(event) {
     event.preventDefault();
 
-let text =event.target.textForm.value;
+// let text =event.target.textForm.value;
 let fisrtName=event.target.firsName.value;
 
 let gender=event.target.gender.value;
 let age = event.target.age.value;  
 
-console.log(text,fisrtName,gender,age );
+console.log(fisrtName,gender,age );
 
 }
